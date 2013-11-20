9bbea49676bd0b442690b80066164a894385b190
Martin <commerce.github@remylabs.com>
2013-11-20 12:16:21 -0600

    Small changes to work with different versions of Kafka and Kafka-Storm

--- src/main/java/org/hackreduce/storm/gnip/bolt/GnipRequestException.java
+++
@@ -1,7 +0,0 @@
-package org.hackreduce.storm.gnip.bolt;
-
-public class GnipRequestException extends Exception {
-  public GnipRequestException(String message, Throwable cause) {
-    super(message, cause);
-  }
-}
--- src/main/java/org/hackreduce/storm/vmc/common/Whatever.java
+++
@@ -1,7 +0,0 @@
-package org.hackreduce.storm.vmc.common;
-
-public class Whatever
-{
-  public final static String HELLOFIELD = "helloField";
-  public final static String LANGUAGEFIELD = "langField";
-}
