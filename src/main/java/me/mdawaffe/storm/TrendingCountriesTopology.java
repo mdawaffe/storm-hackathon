b89d333c43aceee86e3893143f12d3520dfc56aa
Michael D Adams <michael.d.adams@gmail.com>
2013-11-20 11:32:09 -0600

    Pull Projection bolt out into its own file.

--- src/main/java/me/mdawaffe/storm/TrendingCountriesTopology.java
+++ src/main/java/me/mdawaffe/storm/TrendingCountriesTopology.java
@@ -1,13 +1,9 @@
 package me.mdawaffe.storm;
 
-import backtype.storm.topology.base.BaseBasicBolt;
 import backtype.storm.Config;
 import backtype.storm.LocalCluster;
 import backtype.storm.StormSubmitter;
-import backtype.storm.topology.BasicOutputCollector;
-import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.topology.TopologyBuilder;
-import backtype.storm.tuple.Tuple;
 import backtype.storm.tuple.Values;
 import backtype.storm.tuple.Fields;
 import backtype.storm.utils.Utils;
@@ -17,42 +13,19 @@ import storm.starter.bolt.IntermediateRankingsBolt;
 import storm.starter.bolt.TotalRankingsBolt;
 
 import me.mdawaffe.storm.spout.RandomStatsSpout;
+import me.mdawaffe.storm.bolt.ProjectOneStringField;
 
 /**
  * Stats -> Projection by Country -> Trending Ranks
  */
 public class TrendingCountriesTopology {
 
-	/*
-	 * Project a Multi-Field Tuple stream onto a Single-Field Tuple stream
-	 */
-	public static class ProjectOneField extends BaseBasicBolt {
-		String field;
-
-		public ProjectOneField( String field ) {
-			super();
-			this.field = field;
-		}
-
-		@Override
-		public void execute( Tuple tuple, BasicOutputCollector collector ) {
-			String value = tuple.getStringByField( this.field );
-
-			collector.emit( new Values( value ) );
-		}
-		
-		@Override
-		public void declareOutputFields( OutputFieldsDeclarer ofd ) {
-			ofd.declare( new Fields( this.field ) );
-		}
-	}
-
 	public static void main( String[] args ) throws Exception {
 		TopologyBuilder builder = new TopologyBuilder();
 
 		builder.setSpout( "stats", new RandomStatsSpout(), 1 );
 
-		builder.setBolt( "country", new ProjectOneField( "country" ), 1 ).localOrShuffleGrouping( "stats" );
+		builder.setBolt( "country", new ProjectOneStringField( "country" ), 1 ).localOrShuffleGrouping( "stats" );
 
 		builder.setBolt( "counter", new RollingCountBolt( 30, 5 ),      1 ).fieldsGrouping( "country", new Fields( "country" ) );
 		builder.setBolt( "rank",    new IntermediateRankingsBolt( 10 ), 1 ).fieldsGrouping( "counter", new Fields( "obj" ) );
